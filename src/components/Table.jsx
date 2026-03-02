d1c19938498040ab4305ec360246b1ddb5966ce5
Arhises <roma1039@gmail.com>
2026-03-02 11:29:07 +0200

    calc points in progress

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,5 +1,5 @@
 import {useEffect, useState} from "react";
-import {getLeagueById} from "../services/api.js";
+import {getLeagueById, getLeagueHistoryByIdAndTeam} from "../services/api.js";
 import {PlayerList} from "./PlayerList.jsx";
 import {HistoryList} from "./HistoryList.jsx";
 
@@ -7,6 +7,8 @@ export const Table = ({leagueId}) => {
 
     const [league, setLeague] = useState([]);
     const [selectedTeamId, setSelectedTeamId] = useState(null);
+    const [pointsPerTeam, setPointsPerTeam] = useState({});
+    const [goalDiff,setGoalDiff] = useState({});
 
     useEffect(() => {
         const fetchLeague = async () => {
@@ -17,8 +19,24 @@ export const Table = ({leagueId}) => {
             } else {
                 console.error("Error fetching league from API");
             }
+
+            try {
+                const response = await getLeagueHistoryByIdAndTeam(leagueId, teamId);
+                if (response.status === 200) {
+                    setHistory(response.data);
+                    console.log("The history:", response.data);
+                } else {
+                    setError("Error fetching history from API");
+                }
+            } catch (e) {
+                setError(e?.message ?? "Error fetching history from API");
+            } finally {
+                setLoading(false);
+            }
+
         };
         fetchLeague()
+
     }, [leagueId])
 
     const handleTeamClick = (teamId) => {
@@ -29,9 +47,19 @@ export const Table = ({leagueId}) => {
         }
     };
 
+    const countPointsPerTeam = (team) => {
+
+    }
+
     return (
         <section className="Table">
             The Table for league {leagueId}:
+            <section>
+                <label>sort by: </label>
+                <select onChange={null}><option>points</option>
+                <option>goal difference</option>
+                <option>Abc</option></select>
+            </section>
             {league.map((team) => (
                 <div className="TableRow" key={team.id}>
                     <div className="Team">
